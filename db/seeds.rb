c01380d9f0ced2bbc399826fe3cd5a558c22170a
Hendrik Wolleswinkel <hendrikwolleswinkel@me.com>
2017-09-07 15:34:12 +0200

    finished rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized_ingredients = open(url).read
+ingredients = JSON.parse(serialized_ingredients)
+ingredients = ingredients["drinks"]
+
+ingredients.each do |ingredient|
+  Ingredient.create(name: "#{ingredient['strIngredient1']}")
+end
+
+
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "brown sugar")
+# Ingredient.create(name: "cinamon")
+# Ingredient.create(name: "whiskey")
+# Ingredient.create(name: "gin")
+# Ingredient.create(name: "vodka")
+# Ingredient.create(name: "rum")
+# Ingredient.create(name: "tequila")
+# Ingredient.create(name: "cachaca")
+# Ingredient.create(name: "43")
+# Ingredient.create(name: "baileys")
+# Ingredient.create(name: "strawberries")
+# Ingredient.create(name: "cucumber")
+# Ingredient.create(name: "pepper")
+# Ingredient.create(name: "orange rind")
+
